9c4ca354d406f2daf0502bc99a636d8e3c900030
Hosein-Ahmadzade <64315919+Hosein-Ahmadzade@users.noreply.github.com>
2021-03-18 22:02:09 +0330

    generators.py file updated.

--- generators.py
+++ generators.py
@@ -23,7 +23,7 @@ Now if we make a list with input of 100, and then print that list, we get our li
 
 But range is a generator and a generator is a little bit different, because this is not being held in memory. When we do this for loop (Line 33), this range doesn't create on its own a giant list of 0 to 99. And then is it starts iterating? No. It says, with the for loop as a range, I'm going to give you first the number 0 (for i in 0), then I'm going to give you the number 1 (for i in 1), and then 2 and so on all the way to a hundred. So in memory it never ever creates this list (my_list) like we had with make_lsit function.
 
-And notice that this can get pretty hectic. Because when we want to create a giant list with input of 1000000 and then click run, we see that's a lot of memory that we're using up and once it's done, only then we can use this list and it's going to access that list in memory and then we're able to use it. But a more efficient way is to use a generator and actually generate this without taking space in memory.
+And notice that this can get pretty hectic. Because when we want to create a giant list with input of 100000 and then click run, we see that's a lot of memory that we're using up and once it's done, only then we can use this list and it's going to access that list in memory and then we're able to use it. But a more efficient way is to use a generator and actually generate this without taking space in memory.
 
 """
 
@@ -35,13 +35,13 @@ def make_list(num):
     return result
 
 
-# my_list = make_list(100)
+my_list = make_list(100)
 
-# print(my_list)
+print(my_list)
 
-# li = make_list(1000000)
+li = make_list(100000)
 
-# print(li)
+print(li)
 
 
 print("*******************************")
@@ -283,6 +283,17 @@ for i in gen:
 print("*******************************")
 
 
+"""
+
+Exercise Fibonacci Numbers: 
+
+
+Using List And Using Generator:
+
+
+"""
+
+
 def fib(number):
     a, b = 0, 1
     for i in range(number):
@@ -303,11 +314,11 @@ def fib2(number):
     return li
 
 
-# for item in fib(21):
-#     print(item)
+for item in fib(21):
+    print(item)
 
 
-# print(fib2(21))
+print(fib2(21))
 
 
 print("*******************************")
